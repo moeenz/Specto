21433a2c0a12d2b05222f63c3d36fe70196be3e8
Moeen Zamani <moeen.zamani@gmail.com>
2021-02-17 23:02:42 +0330

    Initial Commit

---
+++ Specto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Specto
+//
+//  Created by Moeen Zamani on 2/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Specto/SpectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectoApp.swift
+//  Specto
+//
+//  Created by Moeen Zamani on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SpectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
