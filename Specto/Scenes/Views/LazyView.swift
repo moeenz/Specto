226f9afa5a56127b897aea44132b04315ab5a295
Abbas Mousavi <abbasmousavi@gmail.com>
2021-02-20 12:27:41 +0000

    Adds Icons

--- Specto/Scenes/Views/LazyView.swift
+++ Specto/Scenes/Views/LazyView.swift
@@ -18,3 +18,49 @@ struct LazyView<Content: View>: View {
     }
 }
 
+extension UIView {
+    func asImage() -> UIImage {
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 2
+        return UIGraphicsImageRenderer(size: self.layer.frame.size, format: format).image { context in
+            self.drawHierarchy(in: self.layer.bounds, afterScreenUpdates: true)
+            //layer.render(in: context.cgContext)
+            
+        }
+    }
+}
+
+
+extension View {
+    func asImage(size: CGSize) -> UIImage {
+        let controller = UIHostingController(rootView: self)
+        controller.view.bounds = CGRect(origin: .zero, size: size)
+        //UIApplication.shared.windows.first!.rootViewController?.view.addSubview(controller.view)
+        let image = controller.view.asImage()
+        //controller.view.removeFromSuperview()
+        return image
+    }
+}
+
+struct RectGetter: View {
+    @Binding var rect: CGRect
+    
+    var body: some View {
+        GeometryReader { proxy in
+            self.createView(proxy: proxy)
+        }
+    }
+    
+    func createView(proxy: GeometryProxy) -> some View {
+        DispatchQueue.main.async {
+            self.rect = proxy.frame(in: .global)
+        }
+        
+        return Rectangle().fill(Color.clear)
+    }
+}
+
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
